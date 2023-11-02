892b8a02c4d59553de2095054a744ef1643d588e
DennisMwauraKA <mwaurakariuki70@gmail.com>
2023-11-02 19:11:35 +0300

    updated-the-website

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -5,6 +5,7 @@ function ProjectSection() {
     <div>
       <div>
         <h1 className="text-white text-center">Projects</h1>
+        <p>in here i'll explain all the projects that i have been able to accomplish over the last months</p>
       </div>
     </div>
   );
